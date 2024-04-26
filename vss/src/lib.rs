df184710c8073041c79a6a93a3d19b5be49df652
Yanpei Guo <2847399255@qq.com>
2024-04-26 11:51:11 +0800

    1

--- vss/src/lib.rs
+++ vss/src/lib.rs
@@ -1,18 +1,43 @@
-use util::algebra::{
-    field::Field,
-    polynomial::{self, MultilinearPolynomial, Polynomial},
+use util::{
+    algebra::{
+        coset::Coset,
+        field::Field,
+        polynomial::{self, MultilinearPolynomial, Polynomial},
+    },
+    random_oracle::RandomOracle,
 };
 
 struct Dealer<T: Field> {
     polynomial: MultilinearPolynomial<T>,
+    oracle: RandomOracle<T>,
     lines: Vec<Vec<(T, T)>>,
 }
 
 impl<T: Field> Dealer<T> {
-    pub fn new(polynomial: Polynomial<T>) -> Self {
+    pub fn new(polynomial: Polynomial<T>, oracle: &RandomOracle<T>) -> Self {
         Dealer {
             polynomial: MultilinearPolynomial::new(polynomial.coefficients),
+            oracle: oracle.clone(),
             lines: vec![],
+        }
+    }
+
+    pub fn deal(&mut self) {
+        let log_n = self.polynomial.variable_num() + 1;
+        let coset = Coset::new(1 << log_n, T::from_int(1));
+        let mut sharing = coset.fft(self.polynomial.coefficients().clone());
+        for i in 0..log_n {
+            let mut line = vec![];
+            let len = sharing.len() / 2;
+            for j in 0..len {
+                let k =
+                    (sharing[j] - sharing[j + len]) * T::INVERSE_2 * coset.element_inv_at(j << i);
+                let b = (sharing[j] + sharing[j + len]) * T::INVERSE_2;
+                line.push((k, b));
+                sharing[j] = k * self.oracle.folding_challenges[i] + b;
+            }
+            sharing.truncate(len);
+            self.lines.push(line)
         }
     }
 }
