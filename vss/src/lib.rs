4fcfd9b575687b798bcf245ad8d0eec65944cc1f
Yanpei Guo <2847399255@qq.com>
2024-04-26 16:57:57 +0800

    1

--- vss/src/lib.rs
+++ vss/src/lib.rs
@@ -11,27 +11,38 @@ struct Dealer<T: Field> {
     polynomial: MultilinearPolynomial<T>,
     oracle: RandomOracle<T>,
     lines: Vec<Vec<(T, T)>>,
+    coset: Coset<T>,
+    sharing: Vec<T>,
+    final_value: Option<T>,
 }
 
 impl<T: Field> Dealer<T> {
-    pub fn new(polynomial: MultilinearPolynomial<T>, oracle: &RandomOracle<T>) -> Self {
+    pub fn new(
+        polynomial: MultilinearPolynomial<T>,
+        oracle: &RandomOracle<T>,
+        coset: &Coset<T>,
+    ) -> Self {
+        assert_eq!(coset.size(), 1 << (polynomial.variable_num() + 1));
+        let sharing = coset.fft(polynomial.coefficients().clone());
         Dealer {
             polynomial,
             oracle: oracle.clone(),
             lines: vec![],
+            coset: coset.clone(),
+            sharing,
+            final_value: None,
         }
     }
 
     pub fn deal(&mut self) {
-        let log_n = self.polynomial.variable_num() + 1;
-        let coset = Coset::new(1 << log_n, T::from_int(1));
-        let mut sharing = coset.fft(self.polynomial.coefficients().clone());
+        let mut sharing = self.sharing.clone();
         for i in 0..self.polynomial.variable_num() {
             let mut line = vec![];
             let len = sharing.len() / 2;
             for j in 0..len {
-                let k =
-                    (sharing[j] - sharing[j + len]) * T::INVERSE_2 * coset.element_inv_at(j << i);
+                let k = (sharing[j] - sharing[j + len])
+                    * T::INVERSE_2
+                    * self.coset.element_inv_at(j << i);
                 let b = (sharing[j] + sharing[j + len]) * T::INVERSE_2;
                 line.push((k, b));
                 sharing[j] = k * self.oracle.folding_challenges[i] + b;
@@ -48,10 +59,63 @@ impl<T: Field> Dealer<T> {
                     .folding_challenges
                     .iter()
                     .map(|x| x.clone())
-                    .take(log_n - 1)
                     .collect()
             })
         );
+        self.final_value = Some(sharing[0]);
+    }
+
+    pub fn generate_proof(&self, party: usize) -> Proof<T> {
+        Proof {
+            share: self.sharing[party],
+            lines: self
+                .lines
+                .iter()
+                .map(|x| x[party & (x.len() - 1)])
+                .collect(),
+            final_value: self.final_value.unwrap(),
+        }
+    }
+}
+
+struct Proof<T: Field> {
+    share: T,
+    lines: Vec<(T, T)>,
+    final_value: T,
+}
+
+struct Party<T: Field> {
+    point: T,
+    oracle: RandomOracle<T>,
+}
+
+impl<T: Field> Party<T> {
+    pub fn new(point: T, oracle: &RandomOracle<T>) -> Self {
+        Party {
+            point,
+            oracle: oracle.clone(),
+        }
+    }
+
+    pub fn verify(&self, proof: Proof<T>) {
+        let (share, lines) = (proof.share, proof.lines);
+        let mut point = self.point;
+
+        assert_eq!(share, lines[0].0 * point + lines[0].1);
+        for i in 0..lines.len() {
+            point *= point;
+            if i == lines.len() - 1 {
+                assert_eq!(
+                    lines[i].0 * self.oracle.folding_challenges[i] + lines[i].1,
+                    proof.final_value
+                );
+            } else {
+                assert_eq!(
+                    lines[i].0 * self.oracle.folding_challenges[i] + lines[i].1,
+                    lines[i + 1].0 * point + lines[i + 1].1
+                );
+            }
+        }
     }
 }
 
@@ -63,9 +127,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let poly = MultilinearPolynomial::<Mersenne61Ext>::random_polynomial(10);
+        let poly = MultilinearPolynomial::random_polynomial(10);
         let oracle = RandomOracle::new(10, 10);
-        let mut dealer = Dealer::new(poly, &oracle);
+        let coset = Coset::new(1 << 11, Mersenne61Ext::from_int(1));
+        let mut dealer = Dealer::new(poly, &oracle, &coset);
         dealer.deal();
+        let proof_2000 = dealer.generate_proof(2000);
+        let party_10 = Party::new(coset.element_at(2000), &oracle);
+        party_10.verify(proof_2000);
     }
 }
